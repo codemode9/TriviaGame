a167507930973db8b450e15a8bfbf58619b16617
codemode9 <timourbasegmez@hotmail.com>
2018-02-20 20:30:27 -0500

    jquery linked, some tentative js outline

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+//********** game initialize on load or on key **********/
+
+// ************ Questions array and booleans*************/
+
+questions = ["blablalba", "blablabla",];
+
+//************* for loop or while loop to go through questions*****/
+
+for ( i = 0; i < questions.length; i++) {
+    return questions[i];
+};
+
+//************* user score function ******************/
+
+//*********** timer function (or whatever timer ends up using) ********/
