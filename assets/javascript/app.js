d760630594422e978670e0ec00ffbc5e0df58607
codemode9 <timourbasegmez@hotmail.com>
2018-02-22 17:00:39 -0500

    fixed radio names

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,14 @@
-//********** game initialize on load or on key **********/
-
-// ************ Questions array and booleans*************/
-
-
-
-//************* for loop or while loop to go through questions*****/
-
-for ( i = 0; i < questions.length; i++) {
-    return questions[i];
-};
-
-
-
-
-
-//*********** truefalse?? multiple choice?? *************** */
-
-
-
-//************* user score function ******************/
-
-//*********** timer function (or whatever timer ends up using) ********/
-
-
-
-
-//*************javascript multiple choice. one tick only  *************/
-//************** maybe have one function per question with one radio button set to true and others set to false **************/
+function fun()
+  {         
+var amountCorrect = 0;          
+for(var i = 1; i <= 10; i++) {
+  var radios = document.getElementsByName("quest"+i);
+  for(var x = 0; x < radios.length; x++) {
+    var radio = radios[x];
+    if(radio.value == "correct" && radio.checked) {
+      amountCorrect++;
+    }
+  }
+ }                   
+    alert("Correct Responses: " + amountCorrect);
+  }
