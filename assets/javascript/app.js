a09b890233390eed288273d9f1ed18210d37d5b3
codemode9 <timourbasegmez@hotmail.com>
2018-02-22 20:52:26 -0500

    finally got the reset button to work, also submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,21 @@
 function fun()
   {         
-var amountCorrect = 0;          
+var amountYep = 0;          
 for(var i = 1; i <= 10; i++) {
   var radios = document.getElementsByName("quest"+i);
   for(var x = 0; x < radios.length; x++) {
     var radio = radios[x];
-    if(radio.value == "correct" && radio.checked) {
-      amountCorrect++;
+    if(radio.value == "yep" && radio.checked) {
+      amountYep++;
     }
   }
  }                   
-    alert("Correct Responses: " + amountCorrect);
+    alert("Correct Responses: " + amountYep);
+
   }
+
+  function clearFun() {
+    var ele = document.querySelectorAll("[name='quest1'], [name='quest2'], [name='quest3'], [name='quest4'], [name='quest5'], [name='quest6'], [name='quest7'], [name='quest8'], [name='quest9'], [name='quest10']");
+    for(var i=0;i<ele.length;i++)
+       ele[i].checked = false;  };
+
