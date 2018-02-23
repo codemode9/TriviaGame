4617eed618d8943ae8480d38520a5371d2c0097a
codemode9 <timourbasegmez@hotmail.com>
2018-02-22 22:29:31 -0500

    everything done except making timer viewable on page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,14 @@
-var timeoutID = window.setTimeout(fun, [10000]);
+var timeoutID = window.setTimeout(fun, [50000]);
+
+var n = 50;
+setTimeout(countDown,50000);
+
+function countDown(){
+   n--;
+   if(n > 0){
+      setTimeout(countDown,1000);
+   }
+   $( ".timerino" ).append(n);};
 
 function fun()
   {         
@@ -14,10 +24,4 @@ for(var i = 1; i <= 10; i++) {
  }                   
     alert("Correct Responses: " + amountYep);
 
-  }
-
-  function clearFun() {
-    var ele = document.querySelectorAll("[name='quest1'], [name='quest2'], [name='quest3'], [name='quest4'], [name='quest5'], [name='quest6'], [name='quest7'], [name='quest8'], [name='quest9'], [name='quest10']");
-    for(var i=0;i<ele.length;i++)
-       ele[i].checked = false;  };
-
+  };
